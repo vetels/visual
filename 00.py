fb473e50115da1ff908d86714e0ad287e7e367ae
Vitaly <valanden@gmail.com>
2022-09-19 14:34:18 +0300

    01_main

--- 00.py
+++ 00.py
@@ -13,7 +13,7 @@ city = soup.findAll('div', {'class':'wob_loc q8U8x', 'id':'wob_loc'})
 week = soup.findAll('div', {'class':'wob_dts', 'id':'wob_dts'})
 weather = soup.findAll('span', {'id':'wob_dc'})
 tmpr = soup.findAll('span', {'class':'wob_t q8U8x', 'id':'wob_tm'})
-test
+
 
 city_var = city[0].text #Город
 city_var_list = []
